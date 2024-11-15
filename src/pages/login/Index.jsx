6d2bac6a4ea03ffa53c0b94ca3063d92a27949bb
mnosumon <mdnuruddin378350@gmail.com>
2024-11-15 14:40:11 +0600

    blank comment

--- src/pages/login/Index.jsx
+++ src/pages/login/Index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Index</div>;
+  return (
+    <div>
+      <div className="">
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
