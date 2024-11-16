abeb58d657406facf22b974a8b68d2ca5d66c46d
mnosumon <mdnuruddin378350@gmail.com>
2024-11-16 23:31:00 +0600

    Home page My friend component design done

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -5,15 +5,15 @@ import MyFriend from "../../components/home/MyFriend";
 
 const Home = () => {
   return (
-    <div className="m-3 grid grid-cols-[1fr,2fr] gap-10">
+    <div className="m-3 grid grid-cols-[1fr,2fr] gap-8">
       <div className="p-4 shadow-md rounded-md">
         <AllUser />
       </div>
-      <div className="grid grid-cols-2 gap-5">
-        <div className="px-4 shadow-md rounded-md">
+      <div className="grid grid-cols-2 gap-4">
+        <div className="px-4 shadow-md rounded-md h-fit py-3">
           <FriendsReq />
         </div>
-        <div className="px-8">
+        <div className="px-4 shadow-md rounded-md h-fit py-3">
           <MyFriend />
         </div>
       </div>
