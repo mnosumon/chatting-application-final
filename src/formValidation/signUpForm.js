d47d8ea90ee658ba56df877e1877317ff72d9529
mnosumon <mdnuruddin378350@gmail.com>
2024-11-17 15:44:26 +0600

    Validation part done

--- src/formValidation/signUpForm.js
+++ src/formValidation/signUpForm.js
@@ -3,9 +3,15 @@ import * as Yup from "yup";
 export const signUpForm = Yup.object({
   name: Yup.string()
     .max(20, "Must be 20 characters or less")
-    .required("Required"),
-  email: Yup.string().email("Invalid email address").required("Required"),
+    .required("Pleas enter your name"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Pleas enter your email"),
   password: Yup.string()
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})/,
+      "Pleas use at least one special charecter "
+    )
     .min(6, "Password must be at least 6 characters")
-    .required("Required"),
+    .required("Pleas enter your password"),
 });
