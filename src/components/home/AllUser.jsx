d1b142b7705dccaecfb498398989183d9aecf7a8
mnosumon <mdnuruddin378350@gmail.com>
2024-11-19 18:45:55 +0600

    when accept friend request then show friend

--- src/components/home/AllUser.jsx
+++ src/components/home/AllUser.jsx
@@ -14,9 +14,11 @@ import {
 
 const AllUser = () => {
   const user = useSelector((state) => state.signUpUser.value);
+
   const [users, setUsers] = useState([]);
   const [friendReqList, setFriendReqList] = useState([]);
   const [cancelReq, setCancelReq] = useState([]);
+  const [friends, setFriends] = useState([]);
 
   const db = getDatabase();
 
@@ -62,14 +64,14 @@ const AllUser = () => {
   useEffect(() => {
     const starCountRef = ref(db, "friends/");
     onValue(starCountRef, (snapshot) => {
-      const requestFiend = [];
+      const friend = [];
       snapshot.forEach((item) => {
         const datas = item.val();
-        requestFiend.push(datas.senderID + datas.recieverID);
+        friend.push(datas.senderID + datas.recieverID);
       });
-      setFriendReqList(requestFiend);
+      setFriends(friend);
     });
-  }, [db]);
+  }, [db, user.uid]);
 
   const handleCancelReq = (data) => {
     const cancelReqFilter = cancelReq.find(
@@ -94,7 +96,7 @@ const AllUser = () => {
             placeholder="Search user..."
           />
         </div>
-        {findUser.map((item) => (
+        {findUser?.map((item) => (
           <div key={item.id} className="flex items-center justify-between mt-2">
             <div className="flex items-center gap-3">
               <div className="w-16 h-16 rounded-full overflow-hidden">
@@ -108,8 +110,14 @@ const AllUser = () => {
                 {item.name}
               </h2>
             </div>
-            {friendReqList.includes(user.uid + item.id) ||
-            friendReqList.includes(item.id + user.uid) ? (
+
+            {friends?.includes(user.uid + item.id) ||
+            friends?.includes(item.id + user.uid) ? (
+              <button className="bg-[#4A81D3] text-[#FFF] text-sm font-inter_medium px-5 py-2 rounded-md mr-1">
+                Friend
+              </button>
+            ) : friendReqList?.includes(user.uid + item.id) ||
+              friendReqList?.includes(item.id + user.uid) ? (
               <button
                 onClick={() => handleCancelReq(item)}
                 className="bg-[#4A81D3] text-[#FFF] text-sm font-inter_medium px-5 py-2 rounded-md mr-1"
